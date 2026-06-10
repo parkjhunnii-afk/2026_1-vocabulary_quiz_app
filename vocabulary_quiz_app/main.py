1702d8c29ddb8e8c9c179ea643c17e8c775abbe1
jihun park <park.jhunnii@gmail.com>
2026-06-10 23:52:33 +0900

    [22511753 박지훈](퀴즈 초기화 버튼 추가)

--- vocabulary_quiz_app/main.py
+++ vocabulary_quiz_app/main.py
@@ -1,14 +1,5 @@
 from __future__ import annotations
 
-import sys
-
-absolute_path = r"C:\Users\USER\Documents\2026_1-vocabulary_quiz_app"
-if absolute_path not in sys.path:
-    sys.path.insert(0, absolute_path)
-
-from vocabulary_quiz_app.app import VocabularyQuizApp
-
-
 import tkinter as tk
 
 from vocabulary_quiz_app.app import VocabularyQuizApp
